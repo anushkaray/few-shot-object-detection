b60f3457f81bd95557cf972f460dc03bf469bdce
Anushka Ray <t-rayanushka@microsoft.com>
2022-07-25 15:33:29 +0000

    edited seed file

--- detectron2/detectron2/utils/env.py
+++ detectron2/detectron2/utils/env.py
@@ -31,14 +31,15 @@ def seed_all_rng(seed=None):
     Args:
         seed (int): if None, will use a strong random seed.
     """
-    if seed is None:
-        seed = (
-            os.getpid()
-            + int(datetime.now().strftime("%S%f"))
-            + int.from_bytes(os.urandom(2), "big")
-        )
-        logger = logging.getLogger(__name__)
-        logger.info("Using a generated random seed {}".format(seed))
+    seed = 100
+    # if seed is None:
+    #     seed = (
+    #         os.getpid()
+    #         + int(datetime.now().strftime("%S%f"))
+    #         + int.from_bytes(os.urandom(2), "big")
+    #     )
+    #     logger = logging.getLogger(__name__)
+    #     logger.info("Using a generated random seed {}".format(seed))
     np.random.seed(seed)
     torch.manual_seed(seed)
     random.seed(seed)
